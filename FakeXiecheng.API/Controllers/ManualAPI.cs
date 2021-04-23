f337de5939bb6f9d5d5d43e552fb57fa91363832
zhangguanghuib <=>
2021-04-23 16:34:24 +0800

    add test controller

---
+++ FakeXiecheng.API/Controllers/ManualAPI.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/manualapi")]
+    //public class ManualAPI:ControllerBase
+    //public class ManualAPIController
+    //[Controller]
+    //public class ManualAPI
+    public class ManualAPIController: ControllerBase
+    {
+        // GET: api/<TestAPIController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
--- FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/Startup.cs
@@ -16,6 +16,7 @@ namespace FakeXiecheng.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,16 @@ namespace FakeXiecheng.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/test", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello from test");
+                //});
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapControllers();
             });
         }
     }
