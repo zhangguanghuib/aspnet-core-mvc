dbe00bcb40feaea2d3a8eaf39d5f7da9a0bc271f
zhangguanghuib <=>
2021-04-25 13:45:48 +0800

    add seeding data through Json file

--- FakeXiecheng.API/Database/AppDbContext.cs
+++ FakeXiecheng.API/Database/AppDbContext.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using FakeXiecheng.API.Models;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
 
 namespace FakeXiecheng.API.Database
 {
@@ -21,30 +23,38 @@ namespace FakeXiecheng.API.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TouristRoute>().HasData(
-                new TouristRoute
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "黄山",
-                    Description = "黄山真好玩",
-                    OriginalPrice = 1299,
-                    Features = "<p>吃住行娱乐购买</p>",
-                    Fees = "<p>交通费用自理</p>",
-                    Notes = "<p>小心危险</p>",
-                    CreatedTime = DateTime.UtcNow
-                },
-                 new TouristRoute
-                 {
-                     Id = Guid.NewGuid(),
-                     Title = "华山",
-                     Description = "华山真好玩",
-                     OriginalPrice = 1299,
-                     Features = "<p>吃住行娱乐购买</p>",
-                     Fees = "<p>交通费用自理</p>",
-                     Notes = "<p>小心危险</p>",
-                     CreatedTime = DateTime.UtcNow
-                 }); ;
+            //modelBuilder.Entity<TouristRoute>().HasData(
+            //    new TouristRoute
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Title = "黄山",
+            //        Description = "黄山真好玩",
+            //        OriginalPrice = 1299,
+            //        Features = "<p>吃住行娱乐购买</p>",
+            //        Fees = "<p>交通费用自理</p>",
+            //        Notes = "<p>小心危险</p>",
+            //        CreatedTime = DateTime.UtcNow
+            //    },
+            //     new TouristRoute
+            //     {
+            //         Id = Guid.NewGuid(),
+            //         Title = "华山",
+            //         Description = "华山真好玩",
+            //         OriginalPrice = 1299,
+            //         Features = "<p>吃住行娱乐购买</p>",
+            //         Fees = "<p>交通费用自理</p>",
+            //         Notes = "<p>小心危险</p>",
+            //         CreatedTime = DateTime.UtcNow
+            //     }); 
 
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePicturesJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePicturesJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+            
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ FakeXiecheng.API/Migrations/20210425053927_dataSeedingV2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FakeXiecheng.API.Migrations
+{
+    public partial class dataSeedingV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("106e9a23-681c-4206-a197-c14d58221409"));
+
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("5aee5daf-378f-422c-a9fd-c1f99d009dc0"));
+
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreatedTime", "DepartureTime", "Description", "DiscountPercentage", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("106e9a23-681c-4206-a197-c14d58221410"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "��ɽ�����", 0.10000000000000001, "<p>��ס�����ֹ������</p>", "<p>��ͨ����ȫ������</p>", "<p>С��Σ�պ�ɽ��</p>", 11999.99m, "��ɽ", null });
+
+            migrationBuilder.InsertData(
+                table: "TouristRoutePictures",
+                columns: new[] { "Id", "TouristRouteId", "Url" },
+                values: new object[] { 1, new Guid("106e9a23-681c-4206-a197-c14d58221410"), "../../assets/images/songshan.jpg" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutePictures",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("106e9a23-681c-4206-a197-c14d58221410"));
+
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreatedTime", "DepartureTime", "Description", "DiscountPercentage", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("106e9a23-681c-4206-a197-c14d58221409"), new DateTime(2021, 4, 25, 5, 10, 30, 768, DateTimeKind.Utc).AddTicks(9466), null, "黄山真好玩", null, "<p>吃住行娱乐购买</p>", "<p>交通费用自理</p>", "<p>小心危险</p>", 1299m, "黄山", null });
+
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreatedTime", "DepartureTime", "Description", "DiscountPercentage", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("5aee5daf-378f-422c-a9fd-c1f99d009dc0"), new DateTime(2021, 4, 25, 5, 10, 30, 768, DateTimeKind.Utc).AddTicks(9931), null, "华山真好玩", null, "<p>吃住行娱乐购买</p>", "<p>交通费用自理</p>", "<p>小心危险</p>", 1299m, "华山", null });
+        }
+    }
+}
