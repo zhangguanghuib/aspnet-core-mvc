651ba0039524d125ff5bf62dfc4223cb561a9d4c
zhangguanghuib <=>
2021-04-26 21:12:33 +0800

    xml

--- FakeXiecheng.API/Startup.cs
+++ FakeXiecheng.API/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace FakeXiecheng.API
 {
@@ -26,7 +27,10 @@ namespace FakeXiecheng.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction=> {
+                setupAction.ReturnHttpNotAcceptable = true;
+                setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            });
             services.AddTransient<ITouristRouteRepository, TouristRouteRepository>();
             services.AddDbContext<AppDbContext>(option => {
                 option.UseSqlServer(this.Configuration["DbContext:ConnectionString"]);
