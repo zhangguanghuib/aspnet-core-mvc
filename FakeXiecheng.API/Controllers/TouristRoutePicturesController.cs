a65ec9859c78794c44a281ee002cdbebbc539b49
zhangguanghuib <=>
2021-04-26 23:00:10 +0800

    TouristRoutePictureDto

---
+++ FakeXiecheng.API/Controllers/TouristRoutePicturesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeXiecheng.API.Services;
+using AutoMapper;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/touristRoutes/{touristRouteId}/pictures")]
+    [ApiController]
+    public class TouristRoutePicturesController : ControllerBase
+    {
+        private ITouristRouteRepository _touristRouteRepository;
+        private IMapper _mapper;
+
+        public TouristRoutePicturesController(
+            ITouristRouteRepository touristRouteRepository,
+            IMapper mapper
+            )
+        {
+            _touristRouteRepository = touristRouteRepository ?? throw new ArgumentNullException(nameof(touristRouteRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}
---
+++ FakeXiecheng.API/Profiles/TouristRoutePictureProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FakeXiecheng.API.Models;
+using FakeXiecheng.API.Dtos;
+
+namespace FakeXiecheng.API.Profiles
+{
+    public class TouristRoutePictureProfile : Profile
+    {
+        public TouristRoutePictureProfile()
+        {
+            CreateMap<TouristRoutePicture, TouristRoutePictureDto>();
+        }
+    }
+}
