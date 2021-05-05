363075a2ccbd6c62c089b63a33131c662ad44fba
zhangguanghuib <=>
2021-05-05 21:35:59 +0800

    add TouristRoute and its pictures

--- FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
+++ FakeXiecheng.API/Dtos/TouristRouteForCreationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FakeXiecheng.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,5 +23,6 @@ namespace FakeXiecheng.API.Dtos
         public string TravelDays { get; set; }
         public string TripType { get; set; }
         public string DepartureCity { get; set; }
+        public ICollection<TouristRoutePictureForCreationDto> TouristRoutePictures { get; set; } = new List<TouristRoutePictureForCreationDto>();
     }
 }
