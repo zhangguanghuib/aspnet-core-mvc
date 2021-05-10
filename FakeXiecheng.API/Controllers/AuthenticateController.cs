dae2dcc9cabfc10b33794ee42845790f1956959e
zhangguanghuib <=>
2021-05-10 23:13:48 +0800

    JWT ->generate token

---
+++ FakeXiecheng.API/Controllers/AuthenticateController.cs
@@ -0,0 +1,61 @@
+﻿using FakeXiecheng.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public AuthenticateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult login([FromBody]LoginDto loginDto)
+        {
+            // verify username and password
+
+            // create JWT token
+            // header
+            var signingAlgorithm = SecurityAlgorithms.HmacSha256;
+            // payload
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "fake_user_id")
+            };
+            // signature
+            var secretByte = Encoding.UTF8.GetBytes(_configuration["Authentication:SecretKey"]);
+            var signingKey = new SymmetricSecurityKey(secretByte);
+            var signingCredentials = new SigningCredentials(signingKey, signingAlgorithm);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Authentication:Issuer"],
+                audience: _configuration["Authentication:Audience"],
+                claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials
+                );
+
+            var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
+            // return 200 OK + JWT token
+
+            return Ok(tokenStr);
+        }
+    }
+}
